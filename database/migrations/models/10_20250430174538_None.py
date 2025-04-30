eaa02a95b6c2c0174b6cc30b625be14207eacc3b
Anton ill <gidrontv1@gmail.com>
2025-04-30 17:46:11 +0400

    recreated migrations

--- database/migrations/models/10_20250430174538_None.py
+++ database/migrations/models/10_20250430174538_None.py
@@ -12,13 +12,15 @@ CREATE TABLE IF NOT EXISTS "user" (
     "tg_id" VARCHAR(10) NOT NULL UNIQUE,
     "username" VARCHAR(32),
     "name" VARCHAR(129) NOT NULL,
-    "is_active" BOOL NOT NULL DEFAULT True
+    "is_active" BOOL NOT NULL DEFAULT True,
+    "is_admin" BOOL NOT NULL DEFAULT False,
+    "points" INT NOT NULL DEFAULT 5
 );
 CREATE TABLE IF NOT EXISTS "post" (
     "id" SERIAL NOT NULL PRIMARY KEY,
-    "title" VARCHAR(255) NOT NULL,
-    "url" VARCHAR(300) NOT NULL,
-    "category_id" INT NOT NULL REFERENCES "category" ("id") ON DELETE CASCADE,
+    "content" TEXT NOT NULL,
+    "created_at" TIMESTAMPTZ NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "category_id" INT REFERENCES "category" ("id") ON DELETE SET NULL,
     "user_id" INT NOT NULL REFERENCES "user" ("id") ON DELETE CASCADE
 );
 CREATE TABLE IF NOT EXISTS "aerich" (
